12389b3f4a65ef812e8331e65011c5bf43ca0867
Janek Gröhl <j.groehl@dkfz-heidelberg.de>
2020-03-13 14:39:13 +0100

    added all meta data parameters from document into the list

--- core/metadata_tags.py
+++ core/metadata_tags.py
@@ -28,40 +28,115 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-class MetadataDeviceTags():
+import numpy as np
+
+
+class Units:
+    NO_UNIT = "N/A"
+    DIMENSIONLESS_UNIT = "one"
+    METERS = "m"
+    RADIANS = "rad"
+    JOULES = "J"
+    SECONDS = "s"
+    KELVIN = "K"
+    HERTZ = "Hz"
+
+
+class MetaDatum:
     """
-    Imaging Device Tags.
-    This class
+    This class represents a meta datum.
+    A meta datum contains all necessary information to fully characterize the meta information
+    represented by an instance of this class.
     """
 
-    UUID = "uuid"
-    FIELD_OF_VIEW = "field_of_view"
-    NUMBER_OF_ILLUMINATORS = "num_illuminators"
-    NUMBER_OF_DETECTORS = "num_detectors"
-    ILLUMINATION_ELEMENT = "illumination_element"
-    ILLUMINATOR_POSITION = "illuminator_position"
+    def __init__(self, tag, mandatory, dtype, unit=Units.NO_UNIT):
+        """
 
-    #TODO continue .....
+        :param tag:
+        :param mandatory:
+        :param dtype:
+        :param unit:
+        :raises TypeError if one of the parameters is not of correct type.
+        """
+
+        if tag is not None and isinstance(tag, str):
+            self.tag = tag
+        else:
+            raise TypeError("tag parameter was not of type 'string'")
+
+        if mandatory is not None and isinstance(mandatory, bool):
+            self.mandatory = mandatory
+        else:
+            raise TypeError("mandatory parameter was not of type 'bool'")
+
+        if unit is not None and isinstance(unit, str):
+            self.unit = unit
+        else:
+            raise TypeError("unit parameter was not of type 'string'")
+
+        self.dtype = dtype
 
 
-class MetadataBinaryTags():
+
+class MetadataDeviceTags:
+    """
+    This class defines the naming conventions of the
+    """
+    # General purpose fields
+    UUID = MetaDatum("uuid", True, str)
+    FIELD_OF_VIEW = MetaDatum("field_of_view", False, np.ndarray, Units.METERS)
+    NUMBER_OF_ILLUMINATORS = MetaDatum("num_illuminators", False, int, Units.DIMENSIONLESS_UNIT)
+    NUMBER_OF_DETECTORS = MetaDatum("num_detectors", False, int, Units.DIMENSIONLESS_UNIT)
+
+    # Illumination geometry-specific fields
+    ILLUMINATION_ELEMENT = MetaDatum("illumination_element", False, str)
+    ILLUMINATOR_POSITION = MetaDatum("illuminator_position", False, np.ndarray, Units.METERS)
+    ILLUMINATOR_ORIENTATION = MetaDatum("illuminator_orientation", False, np.ndarray, Units.RADIANS)
+    ILLUMINATOR_SHAPE = MetaDatum("illuminator_shape", False, np.ndarray, Units.METERS)
+    WAVELENGTH_RANGE = MetaDatum("wavelength_range", False, np.ndarray, Units.METERS)
+    LASER_ENERGY_PROFILE = MetaDatum("laser_energy_profile", False, np.ndarray, Units.JOULES)
+    LASER_STABILITY_PROFILE = MetaDatum("laser_stability_profile", False, np.ndarray, Units.JOULES)
+    PULSE_WIDTH = MetaDatum("pulse_width", False, float, Units.SECONDS)
+    BEAM_INTENSITY_PROFILE = MetaDatum("beam_intensity_profile", False, np.ndarray, Units.DIMENSIONLESS_UNIT)
+    BEAM_DIVERGENCE_ANGLES = MetaDatum("beam_divergence_angles", False, float, Units.RADIANS)
+
+    # Detection geometry-specific fields
+    DETECTION_ELEMENT = MetaDatum("detection_element", True, str)
+    DETECTOR_POSITION = MetaDatum("detector_position", True, np.ndarray, Units.METERS)
+    DETECTOR_ORIENTATION = MetaDatum("detector_orientation", False, np.ndarray, Units.RADIANS)
+    DETECTOR_SIZE = MetaDatum("detector_size", False, np.ndarray, Units.METERS)
+    FREQUENCY_RESPONSE = MetaDatum("frequency_response", False, np.ndarray, Units.DIMENSIONLESS_UNIT)
+    ANGULAR_RESPONSE = MetaDatum("angular_response", False, np.ndarray, Units.DIMENSIONLESS_UNIT)
+
+
+class MetadataBinaryTags:
     """
     Binary time series data meta data tags
     """
 
-    DATA_TYPE = "data_type"
-    DIMENSIONALITY = "dimensionality"
-    SIZES = "sizes"
+    UUID = MetaDatum("uuid", True, str)
+    ENCODING = MetaDatum("encoding", True, str)
+    COMPRESSION = MetaDatum("compression", True, str)
+    PHOTOACOUSTIC_IMAGING_DEVICE = MetaDatum("photoacoustic_imaging_device", False, str)
+    DATA_TYPE = MetaDatum("data_type", True, str)
+    DIMENSIONALITY = MetaDatum("dimensionality", True, str)
+    SIZES = MetaDatum("sizes", True, np.ndarray, Units.DIMENSIONLESS_UNIT)
 
-    # TODO continue .....
 
-
-class MetadataAcquisitionTags():
+class MetadataAcquisitionTags:
     """
     Binary time series data meta data tags
     """
 
-    PULSE_LASER_ENERGY = "pulse_laser_energy"
-    FRAME_ACQUISITION_TIMESTAMPS = "frame_acquisition_timestamps"
-
-    #TODO continue .....
+    PULSE_LASER_ENERGY = MetaDatum("pulse_laser_energy", False, np.ndarray, Units.JOULES)
+    FRAME_ACQUISITION_TIMESTAMPS = MetaDatum("frame_acquisition_timestamps", False, np.ndarray, Units.SECONDS)
+    ACQUISITION_OPTICAL_WAVELENGTHS = MetaDatum("acquisition_optical_wavelengths", False, np.ndarray, Units.METERS)
+    TIME_GAIN_COMPNSATION = MetaDatum("time_gain_compensation", False, np.ndarray, Units.DIMENSIONLESS_UNIT)
+    OVERALL_GAIN = MetaDatum("overall_gain", False, float, Units.DIMENSIONLESS_UNIT)
+    ELEMENT_DEPENDENT_GAIN = MetaDatum("element_dependent_gain", False, np.ndarray, Units.DIMENSIONLESS_UNIT)
+    TEMPERATURE_CONTROL = MetaDatum("temperature_control", False, np.ndarray, Units.KELVIN)
+    ACOUSTIC_COUPLING_AGENT = MetaDatum("acoustic_cupling_agent", False, str)
+    SCANNING_METHOD = MetaDatum("scanning_method", False, str)
+    AD_SAMPLING_RATE = MetaDatum("ad_sampling_rate", True, np.ndarray, Units.HERTZ)
+    FREQUENCY_DOMAIN_FILTER = MetaDatum("frequency_domain_filter", False, str)
+    FILTER_THRESHOLD = MetaDatum("filter_threshold", False, np.ndarray, Units.HERTZ)
