107168fe230bb6ed33b6bf878c8fbc53b2bcb9c2
lcmyip <36016188+lcmyip@users.noreply.github.com>
2020-09-28 17:26:00 -0400

    Adding data_loading functions

---
+++ ipasc_tool/api/adapters/LawsonOpticsLab_360_System_File_Converter.py
@@ -0,0 +1,140 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2020, IPASC
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+File converter/adapter
+Converts into IPASC HDF5 format
+
+Lawson Optics Lab
+Lawson Health Research Institute
+Western University
+London, ON, Canada
+
+Created by: Lawrence Yip
+Last Modified 2020-09-25
+"""
+import numpy as np
+
+
+from ipasc_tool import BaseAdapter, MetaDatum
+from ipasc_tool import MetadataAcquisitionTags
+from ipasc_tool import DeviceMetaDataCreator, DetectionElementCreator, IlluminationElementCreator
+
+
+class LOLFileConverter(BaseAdapter):
+
+    def __init__(self, raw_data_folder_path, scan_log_file_path, transducer_map, 
+                 num_scans, signal_inv=True, left_shift=12,
+                 thresholding=0, photodiode=66, CheckAveraging=True, end_remove=80):
+        self.raw_data_folder_path = raw_data_folder_path
+        self.scan_log_file_path = scan_log_file_path
+        self.transducer_map = transducer_map
+        self.signal_inv = signal_inv
+        self.num_scans = num_scans
+        self.left_shift = left_shift
+        self.thresholding = thresholding
+        self.photodiode = photodiode
+        self.CheckAveraging = CheckAveraging
+        self.end_remove = end_remove
+        super().__init__()
+        
+        
+
+    def generate_binary_data(self) -> np.ndarray:
+        return self.data
+
+    def generate_meta_data_device(self) -> dict:
+        device_creator = DeviceMetaDataCreator()
+
+        device_creator.set_general_information(uuid="c771111c-36ba-425d-9f53-84b8ff092059",
+                                               fov=np.asarray([0, 0.0384, 0.0384]))
+
+        start_y_position = 0.00015
+        for y_idx in range(128):
+            cur_y_position = start_y_position + 0.0003 * y_idx
+            detection_element_creator = DetectionElementCreator()
+            detection_element_creator.set_detector_position(np.asarray([0, cur_y_position, 0]))
+            detection_element_creator.set_detector_orientation(np.asarray([0, 0, 1]))
+            detection_element_creator.set_detector_size(np.asarray([0.0003, 0.0003, 0.0003]))
+            detection_element_creator.set_frequency_response(np.stack([np.linspace(700, 900, 100),
+                                                                       np.ones(100)]))
+            detection_element_creator.set_angular_response(np.stack([np.linspace(700, 900, 100),
+                                                                     np.ones(100)]))
+
+            device_creator.add_detection_element("detection_element_" + str(y_idx),
+                                                 detection_element_creator.get_dictionary())
+
+        for y_idx in range(2):
+            illumination_element_creator = IlluminationElementCreator()
+            illumination_element_creator.set_beam_divergence_angles(0.20944)
+            illumination_element_creator.set_wavelength_range(np.asarray([700, 950, 1]))
+            if y_idx == 0:
+                illumination_element_creator.set_illuminator_position(np.asarray([0.0083, 0.0192, -0.001]))
+                illumination_element_creator.set_illuminator_orientation(np.asarray([0, -0.383972, 0]))
+            elif y_idx == 1:
+                illumination_element_creator.set_illuminator_position(np.asarray([-0.0083, 0.0192, -0.001]))
+                illumination_element_creator.set_illuminator_orientation(np.asarray([0, 0.383972, 0]))
+            illumination_element_creator.set_illuminator_shape(np.asarray([0, 0.0245, 0]))
+
+            illumination_element_creator.set_laser_energy_profile(np.stack([np.linspace(700, 900, 100),
+                                                                            np.ones(100)]))
+            illumination_element_creator.set_laser_stability_profile(np.stack([np.linspace(700, 900, 100),
+                                                                               np.ones(100)]))
+            illumination_element_creator.set_pulse_width(7e-9)
+            device_creator.add_illumination_element("illumination_element_" + str(y_idx),
+                                                    illumination_element_creator.get_dictionary())
+
+        return device_creator.finalize_device_meta_data()
+
+    def set_metadata_value(self, metadata_tag: MetaDatum) -> object:
+        if metadata_tag == MetadataAcquisitionTags.UUID:
+            return "TestUUID"
+        elif metadata_tag == MetadataAcquisitionTags.DATA_TYPE:
+            return self.meta['type']
+        elif metadata_tag == MetadataAcquisitionTags.AD_SAMPLING_RATE:
+            return float(self.meta['space directions'][1][1]) / 1000000
+        elif metadata_tag == MetadataAcquisitionTags.ACOUSTIC_COUPLING_AGENT:
+            return "Water"
+        elif metadata_tag == MetadataAcquisitionTags.ACQUISITION_OPTICAL_WAVELENGTHS:
+            return np.asarray([700])
+        elif metadata_tag == MetadataAcquisitionTags.COMPRESSION:
+            return "None"
+        elif metadata_tag == MetadataAcquisitionTags.DIMENSIONALITY:
+            return "2D"
+        elif metadata_tag == MetadataAcquisitionTags.ENCODING:
+            return "raw"
+        elif metadata_tag == MetadataAcquisitionTags.SCANNING_METHOD:
+            return "Freehand"
+        elif metadata_tag == MetadataAcquisitionTags.PHOTOACOUSTIC_IMAGING_DEVICE:
+            return "c771111c-36ba-425d-9f53-84b8ff092059"
+        elif metadata_tag == MetadataAcquisitionTags.SIZES:
+            return np.asarray(self.meta['sizes'])
+        else:
+            return None
---
+++ samples/read_LOL_import_module.py
@@ -0,0 +1,272 @@
+# -*- coding: utf-8 -*-
+"""
+Module to import log, data and transducer position files for PAI128 systems
+
+
+Lawson Optics Lab
+Lawson Health Research Institute
+Western University
+London, ON, Canada
+
+Created by: Lawrence Yip
+Last Modified 2020-09-28
+"""
+
+
+import numpy as np
+from pathlib import Path
+from numba import prange
+import pandas as pd
+import math
+
+
+
+
+def importPAI128(raw_data_filename):
+    """
+    read PAI128 raw pv3 files and settings
+
+    Parameters
+    ----------
+    raw_data_filename : TYPE
+        DESCRIPTION. Filename of the pv3 file to load
+
+    Returns
+    -------
+    UserID : TYPE
+        DESCRIPTION. basically unused
+    DAQsettings : TYPE
+        DESCRIPTION. all the data, metadata and raw data outputted
+    tkdata : TYPE
+        DESCRIPTION. basically unused
+
+    """
+    def fread(fidint, nelements, dtype):
+        if dtype is np.str:
+            dt = np.uint8  # WARNING: assuming 8-bit ASCII for np.str!
+        else:
+            dt = dtype
+        data = np.fromfile(fidint, dt, nelements)
+
+        if np.size(data) == 1:
+            data = np.squeeze(data)
+            if np.size(data) ==1:
+                data = np.squeeze(data)
+        return data
+    
+    fid = open(raw_data_filename, mode="rb")
+
+    version = fread(fid,1,np.int16)
+    UserID = fread(fid,1,np.int16)
+    DAQsettingssize = fread(fid,1,np.int32)
+    
+    DAQsettings = {}
+    
+    for ii in range(DAQsettingssize):
+        DAQsettings_perDAQ = {"NumPoints" : fread(fid,1,np.int32),"NumTriggers" : fread(fid,1,np.int32),
+                           "NumChannels" : fread(fid,1,np.int32)}
+        fread(fid,1,np.int8)
+        DAQsettings_perDAQ["TransferRate"] = fread(fid,1,np.double)
+        DAQsettings_perDAQ["TimeStamp"] = fread(fid,1,np.int32)
+        messagesize = fread(fid,1,np.int32)
+        message = fread(fid,messagesize,np.uint8)
+        datasize = np.uint32(fread(fid,1,np.uint32)/2)
+        DAQsettings_perDAQ["DataPointsCombined"] = fread(fid,datasize,np.uint16)
+                    
+        for jj in range(DAQsettings_perDAQ.get("NumChannels")):
+            reshaped = DAQsettings_perDAQ.get("DataPointsCombined")
+            reshaped = reshaped.reshape(np.uint32(datasize/DAQsettings_perDAQ.get("NumChannels")), 
+                                        DAQsettings_perDAQ.get("NumChannels"), order="F")
+                                  
+            reshaped = np.ndarray.byteswap(reshaped[:,:])
+            reshaped = np.transpose(reshaped)
+            reshaped = np.delete(reshaped,0,1)
+            DAQsettings_perDAQ["DataPoints"] = reshaped
+            
+        DAQsettings[ii] = DAQsettings_perDAQ
+        
+    #Trakstar function
+    tkEnabled = fread(fid,1,np.int8)
+    if tkEnabled != 0:
+        tkdatasize = fread(fid,1,np.int32)
+        tkdata = fread(fid,6,np.double)
+    else:
+        tkdata = []
+        
+    fid.close()
+
+    return UserID, DAQsettings, tkdata
+
+def DAQ128settings2RF(DAQ128settings, CheckAveraging):
+    if (DAQ128settings.get(0, {}).get("NumTriggers") > 1 and CheckAveraging == "True"):
+        RFdata = np.zeros((len(DAQ128settings)*DAQ128settings.get(0, {}).get("NumChannels"), 
+                       DAQ128settings.get(0, {}).get("NumPoints")*256),
+                      dtype = np.single, order="F")
+    else:
+        RFdata = np.zeros((len(DAQ128settings)*DAQ128settings.get(0, {}).get("NumChannels"),
+                       DAQ128settings.get(0, {}).get("NumPoints")*256*DAQ128settings.get(0, {}).get("NumTriggers")),
+                      dtype = np.single, order="F")
+        
+    for ii in DAQ128settings:
+        basen = ii * DAQ128settings.get(ii, {}).get("NumChannels")
+        
+        if  (DAQ128settings.get(ii, {}).get("NumTriggers") > 1 and CheckAveraging == "True"):
+            temp = DAQ128settings.get(ii, {}).get("DataPoints")
+            temp = temp.reshape(DAQ128settings.get(ii, {}).get("NumChannels"),-1, 
+                                DAQ128settings.get(ii, {}).get("NumTriggers"),order="F")
+            temp = np.mean(temp,2)
+            
+        else:
+            temp = DAQ128settings.get(ii, {}).get("DataPoints")
+            
+        RFdata[(basen):(basen + DAQ128settings.get(ii, {}).get("NumChannels")),:] = temp
+        
+    return RFdata
+
+def import_and_process_binary(raw_data_folder_path, num_scans, signal_inv=True, left_shift=12,
+                 thresholding=0, photodiode=65, Averaging=True, end_remove=80, fluence_correc=False):
+    
+    #Determine initial values for sample length, number of channels, etc
+    _, initial_values, _ = importPAI128(Path(raw_data_folder_path, "00000.pv3"))
+    
+    if Averaging == True:
+        imData = np.zeros((num_scans, np.int32(initial_values.get(0, {}).get("NumPoints")*256/initial_values.get(0, {}).get("NumTriggers")), 
+                          initial_values.get(0, {}).get("NumChannels")*len(initial_values)), dtype = np.single, order = "F")
+    else:
+        imData = np.zeros((num_scans, np.int32(initial_values.get(0, {}).get("NumPoints")*256), 
+                          initial_values.get(0, {}).get("NumChannels")*len(initial_values)), dtype = np.single, order = "F")
+    
+    #Template to flip the signals since our transducers are all inverted in polarity
+    reverseGain = -1 * np.ones((num_scans, 1),dtype = np.int, order = "F") 
+    reverseGain[photodiode,:] = 1
+    
+    #Load the signals from the binary file
+    AverageFluence = None
+    do_not_fluence_correct = False
+    for i in range(num_scans):
+        count = str(i)
+        S = Path(count.zfill(5)+".pv3")
+        _, DAQsettings, _ = importPAI128(Path(raw_data_folder_path,S))
+        RFdata = DAQ128settings2RF(DAQsettings,Averaging)
+        
+        #Apply inversion
+        if signal_inv == True:
+            RFdata = RFdata * reverseGain
+            
+        #Remove voltage offset using fourier
+        RFdata = np.transpose(RFdata)
+        for iT in prange(initial_values.get(0, {}).get("NumChannels")*len(initial_values)):
+            RFdata_F = np.fft.fft(RFdata[:,iT])
+            RFdata_F[0] = 0
+            RFdata[:,iT] = np.fft.ifft(RFdata_F)
+        
+        if Averaging == False and initial_values.get(0, {}).get("NumTriggers") != 1:
+            do_not_fluence_correct = True
+            break
+        else:
+            #Remove last measurements (spikes)
+            RFdata[(-end_remove):-1,:] = 0;    
+            RFdata[-1,:] = 0;    
+            #remove some at beginning and shift to align with laser Q-sync delay
+            RFdata[0:left_shift+20,:] = 0;    
+            RFdata = np.roll(RFdata,-left_shift,0)   
+        
+        if thresholding != 0:
+            RFdata[abs(RFdata)<thresholding] = 0
+            
+        if fluence_correc == True and do_not_fluence_correct != False:
+            if AverageFluence != None:
+                break
+            else:
+                AverageFluence = np.max(np.mean(initial_values.get(0, {}).get("DataPoints").get(photodiode)))
+            ShotPower = np.max(RFdata[0:(initial_values.get(0, {}).get("NumPoints")*256/initial_values.get(0, {}).get("NumTriggers")), photodiode])
+            ShotRatio = AverageFluence/ShotPower
+            RFadjusted = RFdata*ShotRatio
+            RFdata = RFadjusted
+        imData[i,:,:] = RFdata
+        
+    return RFdata
+
+def load_scan_log(scan_log_file_path):
+    scan_positions = pd.read_csv(scan_log_file_path) # Load log file
+    R = 254.25 # Set length of arm between end effector and CoR for 360array
+    import datetime
+    time_taken = sum(scan_positions["time taken"[:]])
+    time_taken = str(datetime.timedelta(seconds=time_taken))
+        
+    
+    # Calculate CoR using scan positions, then append only needed values
+    x = pd.Series.to_numpy(scan_positions["x"[:]] - R*np.sin(np.radians(scan_positions["v"[:]]))*np.cos(np.radians(scan_positions["u"[:]])))
+    y = pd.Series.to_numpy(scan_positions["y"[:]] - R*np.sin(np.radians(scan_positions["v"[:]]))*np.sin(np.radians(scan_positions["u"[:]])))
+    z = pd.Series.to_numpy(scan_positions["z"[:]] - R*np.cos(np.radians(scan_positions["v"[:]])))
+    scan_positions_abbrev = np.column_stack((pd.Series.to_numpy(scan_positions["x"[:]]), pd.Series.to_numpy(scan_positions["y"[:]]),
+                                             pd.Series.to_numpy(scan_positions["z"[:]]), pd.Series.to_numpy(scan_positions["u"[:]]),
+                                             pd.Series.to_numpy(scan_positions["v"[:]]), x, y, z))
+    numScans = len(scan_positions_abbrev)
+    transPositionsAllScans = np.zeros((64,3,numScans))
+    for i in range(numScans):
+        transPositionsAllScans[:,:,i] = TransducerRotTrans(scan_positions_abbrev[i,0], scan_positions_abbrev[i,1], scan_positions_abbrev[i,2],
+                                                           scan_positions_abbrev[i,3], scan_positions_abbrev[i,4], scan_positions_abbrev[i,5])
+    return transPositionsAllScans, time_taken
+    
+    
+def TransducerRotTrans(eff_x, eff_y, eff_z, eff_phi, eff_theta, R):
+    """
+    %%version 1
+    Takes the log file coordinates (one at a time, so loop if you want this for multiple scan points), calculates the centre of rotation, and applies the appropriate rotations to match the transducer positions happening in the scan.
+    """
+    x = eff_x - R*np.sin(np.radians(eff_theta))*np.cos(np.radians(eff_phi))
+    y = eff_y - R*np.sin(np.radians(eff_theta))*np.sin(np.radians(eff_phi))
+    z = eff_z - R*np.cos(np.radians(eff_theta))
+    centre_rot = np.array([[x, y, z]])
+    
+    try:
+        import tables
+        file = tables.open_file("20200311_Transducer_Position_Home.mat")
+        transducer_pos_home = file.root.transducer_pos_home[:]
+        centre_rot_home = file.root.centre_rot_home[:]
+    except:
+        import scipy.io as sio
+        homepos = sio.loadmat("20200311_Transducer_Position_Home.mat")
+        transducer_pos_home = homepos.get("transducer_pos_home")
+        centre_rot_home = homepos.get("centre_rot_home")
+            
+    delta_cor = centre_rot - centre_rot_home
+    transducer_pos = transducer_pos_home + delta_cor
+    transducer_pos = np.transpose(transducer_pos)
+    u = [0, 0, 1]
+    transducer_desired_afterphi = AxelRot(transducer_pos, eff_phi, u, centre_rot) #Rotate points for phi, going through centre of rotation
+    u2 = np.squeeze(sph2cart(np.radians(eff_phi-90),0, 1)) #Create vector that is 90 deg from the current position of the array to set as rotation axis
+    transducer_desired_aftertheta = AxelRot(transducer_desired_afterphi, -eff_theta, u2, centre_rot)#Rotate points for theta, going through centre of rotation
+    transducer_pos_new = np.transpose(transducer_desired_aftertheta)
+    return transducer_pos_new
+    
+def sph2cart(azimuth, elevation, r):
+    coselev = np.cos(elevation)
+    x = eval("r * coselev * np.cos(azimuth)")
+    y = eval("r * coselev * np.sin(azimuth)")
+    z = eval("r * np.sin(elevation)")
+    return x, y, z
+
+def AxelRot(transducer_pos, thetadeg, u, centre_rot):
+    v = transducer_pos
+    v = (np.subtract(v,centre_rot.T)).T
+    axis = u
+    theta = np.radians(thetadeg)
+    
+    axis = np.asarray(axis)
+    axis = axis / math.sqrt(np.dot(axis, axis))
+    a = math.cos(theta / 2.0)
+    b, c, d = -axis * math.sin(theta / 2.0)
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+    rotation_matrix = np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
+    Counter = 0
+    v_prime_vec_array = np.zeros((64,3))
+    for i in v:
+        v_prime_vec_array[Counter,:] = np.dot(rotation_matrix, i)
+        Counter = Counter +1
+    v_prime_vec_array = np.transpose(np.add(v_prime_vec_array, centre_rot))
+    return v_prime_vec_array
