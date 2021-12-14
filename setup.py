afef9fbc74cc74bdbbc328ef7d50f8ad4d625854
Janek Grohl <Janek.Grohl@cruk.cam.ac.uk>
2021-12-14 10:01:30 +0000

    amended more meta data

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
 
+with open('VERSION', 'r') as version_file:
+    version = version_file.read()
+
 with open('README.md', 'r') as readme_file:
     long_description = readme_file.read()
 
@@ -8,13 +11,14 @@ with open('requirements.txt', 'r') as requirements_file:
 
 setuptools.setup(
     name="pacfish",
-    version="0.1.4",
+    version=version,
     author="International Photoacoustic Standardisation Consortium (IPASC)",
     description="Photoacoustic Converter for Information Sharing (PACFISH)",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license="BSD-3 Clause License",
+    license="BSD-3-Clause",
     packages=setuptools.find_packages(include=["pacfish", "pacfish.*"]),
     install_requires=requirements,
-    python_requires=">=3.7"
+    python_requires=">=3.7",
+    url="https://github.com/IPASC/PACFISH/"
 )
