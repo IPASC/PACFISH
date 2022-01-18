8b4d7fbe60aa51bf0169b90846f2f92041e15536
Janek Grohl <janek.grohl@cruk.cam.ac.uk>
2022-01-18 10:58:30 +0000

    Refactor MATLAB API. Related to #20

--- pacfish_matlab/example_read_hfd5.m
+++ pacfish_matlab/example_read_hfd5.m
@@ -1,10 +1,12 @@
 % read with a given file in Matlab
-path = '../';
-filename = 'demodata';
+path = 'C:\standardised-image-reconstruction\';
+filename = '1BdSLl4BSxpxXDwWcBKKVV4nHULPe7IS8_ipasc.hdf5';
 file = [path filename];
 
 % Create an instance of the class
-inst = ipasc_tool(file);
+% Option A: use the pa_data constructor
+padata = pacfish.pa_data(file);
 
-% test of a given function
-inst.visualize_device
+% Option B: load the HDF5 file manually and instantiate pa_data that way
+[binary_data, meta_acquisition, meta_device] = pacfish.load_data(file);
+padata_2 = pacfish.pa_data(binary_data, meta_acquisition, meta_device);
