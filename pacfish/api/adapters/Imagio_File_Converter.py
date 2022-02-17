a9d8c577f7d57b88f648538469c2db381ce3765e
Scott Miller <samiller@senomedical.com>
2022-02-17 14:21:37 -0600

    add a bone to the skeleton

--- pacfish/api/adapters/Imagio_File_Converter.py
+++ pacfish/api/adapters/Imagio_File_Converter.py
@@ -4,7 +4,7 @@ from pacfish import BaseAdapter, MetaDatum
 from pacfish import MetadataAcquisitionTags
 from pacfish import DeviceMetaDataCreator, DetectionElementCreator, IlluminationElementCreator
 
-class NrrdFileConverter(BaseAdapter):
+class ImagioFileConverter(BaseAdapter):
     """
     For the Seno Imagio system.
     """
@@ -13,11 +13,50 @@ class NrrdFileConverter(BaseAdapter):
         pass
 
     def generate_binary_data(self) -> np.ndarray:
+        """
+        The binary data is the raw time series data.
+        It is internally stored as an N-dimensional numpy array.
+        The binary data must be formatted the following way:
+
+        [detectors, samples, wavelengths, measurements]
+
+        Return
+        ------
+        np.ndarray
+            A numpy array containing the binary data
+        """
         pass
 
     def generate_device_meta_data(self) -> dict:
+        """
+        Must be implemented to define a digital twin of the photoacoustic imaging device.
+        This method can be implemented using the DeviceMetaDataCreator.
+
+        Return
+        ------
+        dict
+            A dictionary containing all key-value pair necessary to describe a digital twin of a
+            photoacoustic device.
+        """
         pass
 
     def set_metadata_value(self, metadatum: MetaDatum) -> object:
+        """
+
+        This method must be implemented to yield appropriate data for all MetaDatum elements in the
+        MetadataTags class.
+
+        You are given a certain meta datum nd have to return the appropriate information.
+
+        Parameters
+        ----------
+        metadatum: MetaDatum
+            The MetaDatum for which to return the correct data.
+
+        Return
+        ------
+        object
+            The data corresponding to the given MetaDatum
+        """
         pass
 
